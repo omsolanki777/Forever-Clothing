128d92c8d848a7e68122073baec15f02b2e3927c
Om Solanki <omsolanki3005@gmail.com>
2025-08-17 10:35:49 +0000

    aos added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,16 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  duration: 1000,
+  once: false,   // animate every time
+  mirror: true,  //  play reverse animation when scrolling back up
+});
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ShopContextProvider>
