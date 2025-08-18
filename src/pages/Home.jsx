87a08eb5c1b7cc925dac553e91aaf657608372ef
Om Solanki <omsolanki3005@gmail.com>
2025-08-18 15:42:24 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,30 @@ import NewsletterBox from '../components/NewsletterBox'
 const Home = () => {
   return (
     <div>
-      <Hero />
-      <LatestCollection />
-      <BestSeller />
-      <OurPolicy />
-      <NewsletterBox />
+      {/* Hero Section (Smooth classic) */}
+      <div data-aos="fade-down" data-aos-duration="1200">
+        <Hero />
+      </div>
+
+      {/* Latest Collection (Smooth classic) */}
+      <div data-aos="fade-up" data-aos-duration="1000">
+        <LatestCollection />
+      </div>
+
+      {/* Best Seller (Different & dynamic) */}
+      <div data-aos="flip-left" data-aos-duration="1000">
+        <BestSeller />
+      </div>
+
+      {/* Our Policy (Different diagonal entry) */}
+      <div data-aos="fade-up-right" data-aos-duration="1000">
+        <OurPolicy />
+      </div>
+
+      {/* Newsletter (Different stylish exit) */}
+      <div data-aos="zoom-out-up" data-aos-duration="1000">
+        <NewsletterBox />
+      </div>
     </div>
   )
 }
